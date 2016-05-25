3d329f8b778df19f5ea85cce8117ffee1940832a
Sabine Miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2016-05-25 11:39:20 +0200

    moved issues in space

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+puts "Hello Stonehenge!"
+puts "Hello Stonehenge!"
+puts "Hello Stonehenge!"
+puts "Hello Stonehenge!"
+puts "Hello Stonehenge!"
+
 def dull_adder(a,b,c,d,e,f)
   return a+b+c+d+e+f
 end
